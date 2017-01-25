e07aecf79355bdd25f7a16d22574f54e6d27e2c4
W540 <W540@LENOVO>
2017-01-25 12:56:33 +0200

    Add project files.

---
+++ Calc2/Calc1DemoSteps.cs
@@ -0,0 +1,118 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using TechTalk.SpecFlow;
+using TestStack.White;
+using TestStack.White.UIItems;
+using TestStack.White.UIItems.Finders;
+using TestStack.White.UIItems.WindowItems;
+
+namespace Calc2
+{
+    [Binding]
+    public class Calc1DemoSteps
+    {
+        private Window _mainWindow;
+
+
+        
+        private string ConvertToName(string s)
+        {
+
+            if (s == "+")
+            {
+                return "Add";
+            }
+           
+            if (s == ",")
+            {
+                return "Decimal separator";
+            }
+
+            if (s == "-")
+            {
+                return "Subtract";
+            }
+
+            if (s == "*")
+            {
+                return "Multiply";
+            }
+
+            if (s == "/")
+            {
+                return "Divide";
+            }
+            return s;
+
+            }
+        
+
+        [Given(@"I have opened calculator")]
+        public void GivenIHaveOpenedCalculator()
+        {
+            var app = Application.Launch("calc.exe");
+             _mainWindow = app.GetWindow("Calculator");
+            
+        }
+
+        [After]
+        public void CloseCalculator()
+        {
+            _mainWindow.Close();
+        }
+
+        [Given(@"I have entered (.*) into the calculator")]
+        public void GivenIHaveEnteredIntoTheCalculator(string p0)
+        {
+            decimal dummy;
+            // if it's not a number, find the whole button name...
+            if (!decimal.TryParse(p0,out dummy))
+            {
+
+                var button = _mainWindow.Get<Button>(ConvertToName(p0));
+                //var criteria = SearchCriteria.ByClassName("Button");
+                //var button = _mainWindow.GetMultiple(criteria);
+                button.Click();
+            }
+            else
+            {
+                // it is a number, press each digit seperately....
+                foreach (var number in p0)
+                {
+                    var button = _mainWindow.Get<Button>(ConvertToName(number.ToString()));
+                    //var criteria = SearchCriteria.ByClassName("Button");
+                    //var button = _mainWindow.GetMultiple(criteria);
+                    button.Click();
+                }
+            }   
+        }       
+
+        [When(@"I press (.*)")]
+        public void WhenIPressEquals(string p0)
+        {
+            
+           //var criteria = SearchCriteria.ByClassName("Button");
+            //var button = _mainWindow.GetMultiple(criteria);
+            var button = _mainWindow.Get<Button>(ConvertToName(p0));
+            button.Click();
+            //ScenarioContext.Current.Pending();
+        }
+
+        [Then(@"the result should be (.*) on the screen")]
+        public void ThenTheResultShouldBeOnTheScreen(string p0)
+        {
+
+            // find this window....
+            //var criteria = SearchCriteria.ByClassName("Static");
+            //var display = _mainWindow.GetMultiple(criteria);
+
+            var criteria = SearchCriteria.ByAutomationId("150");
+            var display = _mainWindow.Get(criteria);
+            Assert.AreEqual(display.Name, p0);
+
+            // read the text.
+            // Assert text == p0;
+            // ScenarioContext.Current.Pending();
+        }
+    }
+}
---
+++ Calc2/Feature/Calc1Demo.feature
@@ -0,0 +1,22 @@
+﻿Feature: Calc1Demo
+	I want to test the sum of two numbers
+	Test the addition/subtraction/multiplication/division of two numbers
+	also use decimal numbers
+
+Scenario Outline: Test basic operations on two numbers
+	Given I have opened calculator
+	And I have entered <value1> into the calculator
+	And I have entered <operator> into the calculator
+	And I have entered <value2> into the calculator
+	When I press Equals
+	Then the result should be <result> on the screen
+
+	Examples: 
+	| value1 | operator | value2 | result |
+	| 30     | +        | 60     | 90     |
+	| 7,5    | +        | 3      | 10,5   |
+	| 4      | -        | 1      | 3      |
+	| 35     | *        | 2      | 70     |
+	| 4      | /        | 2      | 2      |
+	| 5      | -        | 6      | -1     |
+	
